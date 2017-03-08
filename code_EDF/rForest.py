a0c9ccc6aea8017fbee09f45da717eca458dd115
Gaétan <gaetanmorand@MacBook-Pro-de-Gaetan.local>
2017-03-08 14:21:07 +0100

    Updated random forest

--- code_EDF/rForest.py
+++ code_EDF/rForest.py
@@ -1,7 +1,9 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
 
+lb = LabelEncoder()
 
 # EDF data
 tr_input = pd.read_csv("../data_EDF/training_inputs.csv", sep = ";")
@@ -9,19 +11,32 @@ output = pd.read_csv("../data_EDF/challenge_output_data_training_file_predict"
                      "_which_clients_reduced_their_consumption.csv", sep = ";")
 data_edf = pd.merge(left=tr_input, right=output, how='left', on="ID")
 
-to_drop = ['S1', 'S6', 'S7', 'Q6', 'Q7', 'Q15', 'Q17', 'Q18', 'Q37', 'Q38', 'Q39', 'Q40', 'Q52', 'Q73', 'Q74', 'Q75']
-data_edf.drop(to_drop,inplace=True,axis=1).dropna()
+#S3-S7 : dates
+to_drop = ['ID', 'COD_INSEE', 'COD_IRIS', 'S1', 'S3', 'S4', 'S5', 'S6', 'S7', 'Q6', 'Q7', 'Q15', 'Q17', 'Q18', 'Q37', 'Q38', 'Q39', 'Q40', 'Q52', 'Q73', 'Q74', 'Q75']
+mask = ~data_edf.columns.isin(to_drop)
+X = data_edf.loc[:,mask]
 
+#data_edf.drop(to_drop,inplace=True,axis=1)
+X.dropna()
+
+categorical = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C12', 'C13', 'C14', 'C15', 'S2', 'Q1', 'Q2', 'Q3', 'Q8', 'Q10', 'Q11', 'Q12', 'Q16', 'Q21', 'Q23', 'Q24', 'Q25', 'Q26', 'Q27', 'Q28', 'Q29', 'Q32', 'Q34', 'Q36', 'Q53', 'Q54', 'Q55', 'Q56', 'Q57', 'Q58', 'Q59', 'Q60', 'Q61', 'Q62', 'Q63', 'Q64', 'Q65', 'Q66', 'Q67', 'Q68', 'Q69', 'Q70', 'Q71', 'Q72']
+
+#categorical = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C12', 'C13', 'C14', 'C15', 'S2', 'Q1', 'Q2', 'Q3', 'Q7', 'Q8', 'Q10', 'Q11', 'Q12', 'Q16', 'Q21', 'Q23', 'Q24', 'Q25', 'Q26', 'Q27', 'Q28', 'Q29', 'Q32', 'Q34', 'Q36', 'Q39', 'Q53', 'Q54', 'Q55', 'Q56', 'Q57', 'Q58', 'Q59', 'Q60', 'Q61', 'Q62', 'Q63', 'Q64', 'Q65', 'Q66', 'Q67', 'Q68', 'Q69', 'Q70', 'Q71', 'Q72', 'Q73', 'Q74', 'Q75']
+
+for a in categorical:
+	X[a] = X[a].astype('category')
+	X[a] = lb.fit_transform(X[a])
 # ENEDIS data
 #data_enedis = pd.read_csv("../data_EDF/consommation-electrique-par-secteurs-dactivite.csv", sep = ";")
 
 
-data_edf['is_train'] = np.random.uniform(0, 1, len(data_edf)) <= .75
-data_edf['TARGET'] = data_edf['TARGET'].astype('category')
+X['is_train'] = np.random.uniform(0, 1, len(X)) <= .75
 
-train, test = data_edf[data_edf['is_train']==True], data_edf[data_edf['is_train']==False]
+train, test = X[X['is_train']==True], X[X['is_train']==False]
 
-features 
+# features = ['C1', 'C2', 'C3']
+features = list(X)[:-1]
+
 clf = RandomForestClassifier(n_jobs=2)
 y, _ = pd.factorize(train['TARGET'])
 clf.fit(train[features], y)
