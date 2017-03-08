a66c054f5ce8dd700547bfcd616f785435c2ba8b
hippolytej <hippolyte.jacomet@gmail.com>
2017-03-08 11:32:39 +0100

    updated merge code

--- code_EDF/preprocessing_EDF.py
+++ code_EDF/preprocessing_EDF.py
@@ -7,9 +7,12 @@ tr_input = pd.read_csv("../data_EDF/training_inputs.csv", sep = ";")
 output = pd.read_csv("../data_EDF/challenge_output_data_training_file_predict"
                      "_which_clients_reduced_their_consumption.csv", sep = ";")
 data_edf = pd.merge(left=tr_input, right=output, how='left', on="ID")
+data_edf['COD_IRIS'] = data_edf['COD_IRIS'].fillna(0).apply(int)
+data_edf.shape
 
 # ENEDIS data
 data_enedis = pd.read_csv("../data_EDF/consommation-electrique-par-secteurs-dactivite.csv", sep = ";")
+data_enedis.shape
 
 # Clean it
 to_keep = ['Code IRIS',
@@ -18,6 +21,11 @@ to_keep = ['Code IRIS',
            'Conso moyenne secteur résidentiel Enedis (MWh)']
 data_enedis_kept = data_enedis[to_keep]
 data_enedis_kept = data_enedis_kept.dropna()
+data_enedis_kept.shape
+data_enedis_kept = data_enedis_kept[data_enedis_kept['Code IRIS'].str.find('x')==-1]
+data_enedis_kept['Code IRIS'] = data_enedis_kept['Code IRIS'].apply(int)
+data_enedis_kept.columns = ['COD_IRIS', 'nb_sites', 'conso_tot', 'conso_moy']
 
 # Merge'em
-data = pd.merge(left=data_edf, right=data_enedis_kept, how='left', left_on="COD_INSEE", right_on="Code IRIS")
+data = pd.merge(left=data_edf, right=data_enedis_kept, how='left', on="COD_IRIS")
+data.shape
