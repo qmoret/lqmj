1fa5940605619630165b8e6240e5f0292becc285
hippolytej <hippolyte.jacomet@gmail.com>
2017-03-09 01:55:56 +0100

    'lasso'

--- code_EDF/ensemble.py
+++ code_EDF/ensemble.py
@@ -11,13 +11,14 @@ pylab.ion()
 
 Xx = pd.read_csv("xgboo.csv", index_col=0)
 Xr = pd.read_csv("rf.csv", index_col=0)
+Xl = pd.read_csv("las.csv", index_col=0)
 y = pd.read_csv("../data_EDF/challenge_output_data_training_file_predict_which_"
                      "clients_reduced_their_consumption.csv", sep = ";",
                      usecols = [1])
 y = y['TARGET']
 
-alpha = 0.5
-yprob = alpha*Xx['xgb_proba']+(1-alpha)*Xr["rf_proba"]
+
+yprob = 0.4*Xx['xgb_proba']+0.5*Xr["rf_proba"]+0.1*Xl["las"]
 
 fpr, tpr, tres = metrics.roc_curve(y, yprob, pos_label = 1)
 auc = metrics.auc(fpr, tpr)
